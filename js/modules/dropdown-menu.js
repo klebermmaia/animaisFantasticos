ac73ac4563f80c25565bbf6c6f2b5af182a0d9bc
klebermmaia <klebermmaia@gmail.com>
2022-10-01 21:25:14 -0300

    Main

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,18 @@
-export default function initDropDownMenu(){
+import outsideClick from './outsideclick.js';
+export default function initDropDownMenu() {
+  const dropDownMenus = document.querySelectorAll('[data-dropdown]');
 
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('active')
+    outsideClick(this, ['touchstart', 'click'], ()=>{
+      this.classList.remove('active')
+    });
+  }
+
+  dropDownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+      menu.addEventListener(userEvent, handleClick);
+    })
+  })
 } 
--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ import initalTabNavAnimais from './modules/tab-nav.js';
 import arccordion from './modules/anime-scroll.js';
 import animeScroll from './modules/anime-scroll.js';
 import scrollSuave from './modules/scroll-suave.js';
-import initModal from './modules/modal.js'
-import initTooltip from './modules/tooltip.js'
-import initDropDownMenu from './modules/dropdown-menu.js'
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropDownMenu from './modules/dropdown-menu.js';
 
 initalTabNavAnimais();
 arccordion();
