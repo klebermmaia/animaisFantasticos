674375ccd88c329d69664e49a0193fd1393a1b84
klebermmaia <klebermmaia@gmail.com>
2022-10-05 18:38:32 -0300

    Nova função

--- js/modules/anime-scroll.js
+++ js/modules/anime-scroll.js
@@ -1,18 +1,19 @@
 export default function animeScroll() {
-    const sections = document.querySelectorAll('[data-anime="scroll"]');
-    const windowHeight = window.innerHeight * 0.6;
-    if (sections.length) {
-        function animeScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top;
-                const sectionVisible = (sectionTop - windowHeight) < 0;
-                if (sectionVisible)
-                    section.classList.add('animeAtivo');
-                else
-                    section.classList.remove('animeAtivo')
-            })
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  const windowHeight = window.innerHeight * 0.6;
+  if (sections.length) {
+    function animeScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const sectionVisible = (sectionTop - windowHeight) < 0;
+        if (sectionVisible)
+          section.classList.add('animeAtivo');
+        else if (section.classList.contains('animeAtivo')) {
+          section.classList.remove('animeAtivo')
         }
-        animeScroll()
-        window.addEventListener('scroll', animeScroll)
+      })
     }
+    animeScroll()
+    window.addEventListener('scroll', animeScroll)
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 import initalTabNavAnimais from './modules/tab-nav.js';
-import arccordion from './modules/anime-scroll.js';
+import arccordion from './modules/arccordion.js';
 import animeScroll from './modules/anime-scroll.js';
 import scrollSuave from './modules/scroll-suave.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
 
 initalTabNavAnimais();
 arccordion();
@@ -13,3 +15,5 @@ scrollSuave();
 initModal();
 initTooltip();
 initDropDownMenu();
+initMenuMobile();
+initAnimaNumeros();
